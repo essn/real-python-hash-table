7f5b852604d7f1702a07531e211d3fdc4b8cc68e
Jesse Pledger <jpledger@datalot.com>
2022-08-21 13:36:45 -0400

    Add capacity and use the slot concept instead of pairs

--- test_hashtable.py
+++ test_hashtable.py
@@ -15,12 +15,12 @@ def hash_table():
 
 
 def test_should_delete_key_value_pair(hash_table):
-    assert ("hola", "hello") in hash_table.pairs
+    assert ("hola", "hello") in hash_table.slots
     assert len(hash_table) == 3
 
     del hash_table["hola"]
 
-    assert ("hola", "hello") not in hash_table.pairs
+    assert ("hola", "hello") not in hash_table.slots
     assert len(hash_table) == 3
 
 
@@ -65,20 +65,20 @@ def test_should_create_hashtable():
     assert HashTable(capacity=100) is not None
 
 
-def test_should_report_length_of_empty_pairs():
+def test_should_report_length_of_empty_slots():
     assert len(HashTable(capacity=100)) == 0
 
 
-def test_should_insert_key_value_pairs():
+def test_should_insert_key_value_slots():
     hash_table = HashTable(capacity=100)
 
     hash_table["hola"] = "hello"
     hash_table[98.6] = 37
     hash_table[False] = True
 
-    assert ("hola", "hello") in hash_table.pairs
-    assert (98.6, 37) in hash_table.pairs
-    assert (False, True) in hash_table.pairs
+    assert ("hola", "hello") in hash_table.slots
+    assert (98.6, 37) in hash_table.slots
+    assert (False, True) in hash_table.slots
 
     assert len(hash_table) == 3
 
@@ -88,13 +88,13 @@ def test_should_not_contain_none_value_when_created():
 
 
 def test_should_create_empty_value_slots():
-    assert HashTable(capacity=3)._pairs == [None, None, None]
+    assert HashTable(capacity=3)._slots == [None, None, None]
 
 
 def test_should_insert_none_value():
     hash_table = HashTable(capacity=100)
     hash_table["key"] = None
-    assert ("key", None) in hash_table.pairs
+    assert ("key", None) in hash_table.slots
 
 
 def test_should_raise_key_error_when_deleting(hash_table):
@@ -114,17 +114,17 @@ def test_should_update_value(hash_table):
     assert len(hash_table) == 3
 
 
-def test_should_return_pairs(hash_table):
-    assert ("hola", "hello") in hash_table.pairs
-    assert (98.6, 37) in hash_table.pairs
-    assert (False, True) in hash_table.pairs
+def test_should_return_slots(hash_table):
+    assert ("hola", "hello") in hash_table.slots
+    assert (98.6, 37) in hash_table.slots
+    assert (False, True) in hash_table.slots
 
 
-def test_should_return_copy_of_pairs(hash_table):
+def test_should_return_copy_of_slots(hash_table):
     assert hash_table.values is not hash_table.values
 
 
-def test_should_not_include_blank_pairs(hash_table):
+def test_should_not_include_blank_slots(hash_table):
     assert None not in hash_table.values
 
 
@@ -160,19 +160,41 @@ def test_should_return_copy_of_keys(hash_table):
     assert hash_table.keys is not hash_table.keys
 
 
-def test_should_return_pairs(hash_table):
-    assert hash_table.pairs == {("hola", "hello"), (98.6, 37), (False, True)}
+def test_should_return_slots(hash_table):
+    assert hash_table.slots == {("hola", "hello"), (98.6, 37), (False, True)}
 
 
-def test_should_get_pairs_of_empty_hash_table():
-    assert HashTable(capacity=100).pairs == set()
+def test_should_get_slots_of_empty_hash_table():
+    assert HashTable(capacity=100).slots == set()
 
 
 def test_should_convert_to_dict(hash_table):
-    dictionary = dict(hash_table.pairs)
+    dictionary = dict(hash_table.slots)
     assert set(dictionary.keys()) == hash_table.keys
-    assert set(dictionary.items()) == hash_table.pairs
+    assert set(dictionary.items()) == hash_table.slots
     assert list(dictionary.values()) == unordered(hash_table.values)
+
+
+def test_should_not_create_hashtable_with_zero_capacity():
+    with pytest.raises(ValueError):
+        HashTable(capacity=0)
+
+
+def test_should_not_create_hashtable_with_negative_capacity():
+    with pytest.raises(ValueError):
+        HashTable(capacity=-100)
+
+
+def test_should_report_length(hash_table):
+    assert len(hash_table) == 3
+
+
+def test_should_report_capacity_of_empty_hash_table():
+    assert HashTable(capacity=100).capacity == 100
+
+
+def test_should_report_capacity(hash_table):
+    assert hash_table.capacity == 100
 
 
 @pytest.mark.skip
